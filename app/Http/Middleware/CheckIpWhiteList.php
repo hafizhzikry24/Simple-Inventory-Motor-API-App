a6174aae1951a77bf618e316aef14f192785bf01
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2024-10-05 14:13:24 +0700

    [add] telegram bot

--- app/Http/Middleware/CheckIpWhiteList.php
+++ app/Http/Middleware/CheckIpWhiteList.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use App\Models\IpWhiteList;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckIpWhiteList
@@ -22,6 +24,7 @@ class CheckIpWhiteList
         $Ipwhitelists = IpWhiteList::where('ip_address', $ipAddress)->first();
 
         if(!$Ipwhitelists){
+            $this->sendTelegramNotification($ipAddress);
             return response()->json([
                 'status' => 'error',
                 'message' => 'Access denied. You are not authorized to access this resource.',
@@ -30,5 +33,18 @@ class CheckIpWhiteList
         }
 
         return $next($request);
+    }
+
+    private function sendTelegramNotification($ip){
+        $message = "⚠️ Unauthorized IP Detected: ". $ip;
+
+        $response = Http::post("https://api.telegram.org/bot" . env('TELEGRAM_BOT_TOKEN') . "/sendMessage", [
+            'chat_id' => env('TELEGRAM_CHAT_ID'),
+            'text' => $message
+        ]);
+
+        if ($response->failed()) {
+            Log::error("Failed to send Telegram message: " . $response->body());
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -27,40 +27,50 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 
-Route::middleware(['api', 'ip-whitelists'])->prefix('users')->group(function () {
-    Route::get('', [UserController::class, 'index']);
-    Route::get('{id}', [UserController::class, 'show']);
-    Route::post('', [UserController::class, 'store']);
-    Route::put('{id}', [UserController::class, 'update']);
-    Route::delete('{id}', [UserController::class, 'destroy']);
+Route::middleware(['api', 'ip-whitelists'])->group(function () {
+    Route::prefix('users')->group(function () {
+        Route::get('', [UserController::class, 'index']);
+        Route::get('{id}', [UserController::class, 'show']);
+        Route::post('', [UserController::class, 'store']);
+        Route::put('{id}', [UserController::class, 'update']);
+        Route::delete('{id}', [UserController::class, 'destroy']);
+    });
+
+    Route::prefix('motors')->group(function () {
+        Route::get('', [MotorController::class, 'index']);
+        Route::get('{id}', [MotorController::class, 'show']);
+        Route::post('', [MotorController::class, 'store']);
+        Route::put('{id}', [MotorController::class, 'update']);
+        Route::delete('{id}', [MotorController::class, 'destroy']);
+    });
+
+    Route::prefix('mobils')->group(function () {
+        Route::get('', [MobilController::class, 'index']);
+        Route::get('{id}', [MobilController::class, 'show']);
+        Route::post('', [MobilController::class, 'store']);
+        Route::put('{id}', [MobilController::class, 'update']);
+        Route::delete('{id}', [MobilController::class, 'destroy']);
+    });
+
+    Route::prefix('offices')->group(function () {
+        Route::get('', [OfficeController::class, 'index']);
+        Route::get('{id}', [OfficeController::class, 'show']);
+        Route::post('', [OfficeController::class, 'store']);
+        Route::put('{id}', [OfficeController::class, 'update']);
+        Route::delete('{id}', [OfficeController::class, 'destroy']);
+    });
+
+    Route::prefix('sparepartmotors')->group(function () {
+        Route::get('', [SparePartMotorController::class, 'index']);
+        Route::post('', [SparePartMotorController::class, 'store']);
+        Route::get('/{id}', [SparePartMotorController::class, 'show']);
+        Route::put('/{id}', [SparePartMotorController::class, 'update']);
+        Route::delete('/{id}', [SparePartMotorController::class, 'destroy']);
+        Route::post('/import', [SparePartMotorController::class, 'import']);
+        Route::post('/export', [SparePartMotorController::class, 'export']);
+    });
 });
 
-
-Route::middleware(['api', 'ip-whitelists'])->prefix('motors')->group(function () {
-    Route::get('', [MotorController::class, 'index']);
-    Route::get('{id}', [MotorController::class, 'show']);
-    Route::post('', [MotorController::class, 'store']);
-    Route::put('{id}', [MotorController::class, 'update']);
-    Route::delete('{id}', [MotorController::class, 'destroy']);
-});
-
-
-Route::middleware(['api', 'ip-whitelists'])->prefix('mobils')->group(function () {
-    Route::get('', [MobilController::class, 'index']);
-    Route::get('{id}', [MobilController::class, 'show']);
-    Route::post('', [MobilController::class, 'store']);
-    Route::put('{id}', [MobilController::class, 'update']);
-    Route::delete('{id}', [MobilController::class, 'destroy']);
-});
-
-
-Route::middleware(['api', 'ip-whitelists'])->prefix('offices')->group(function () {
-    Route::get('', [OfficeController::class, 'index']);
-    Route::get('{id}', [OfficeController::class, 'show']);
-    Route::post('', [OfficeController::class, 'store']);
-    Route::put('{id}', [OfficeController::class, 'update']);
-    Route::delete('{id}', [OfficeController::class, 'destroy']);
-});
 
 Route::middleware(['api'])->prefix('ip-whitelists')->group(function () {
     Route::get('', [IPWhitelistController::class, 'index']);
@@ -70,14 +80,4 @@ Route::middleware(['api'])->prefix('ip-whitelists')->group(function () {
     Route::delete('/{id}', [IPWhitelistController::class, 'destroy']);
 });
 
-Route::middleware(['api', 'ip-whitelists'])->prefix('sparepartmotors')->group(function () {
-    Route::get('', [SparePartMotorController::class, 'index']);
-    Route::post('', [SparePartMotorController::class, 'store']);
-    Route::get('/{id}', [SparePartMotorController::class, 'show']);
-    Route::put('/{id}', [SparePartMotorController::class, 'update']);
-    Route::delete('/{id}', [SparePartMotorController::class, 'destroy']);
-    Route::post('/import', [SparePartMotorController::class, 'import']);
-    Route::post('/export', [SparePartMotorController::class, 'export']);
-
-});
 
