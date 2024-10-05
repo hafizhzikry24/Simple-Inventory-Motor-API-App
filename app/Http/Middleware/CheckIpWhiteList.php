c5da0eefe36c0ddb634e3e9cd282a0e154d447d9
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2024-10-05 14:36:23 +0700

    [fix]looping-notification-to-telegram-bot

--- app/Http/Middleware/CheckIpWhiteList.php
+++ app/Http/Middleware/CheckIpWhiteList.php
@@ -5,26 +5,35 @@ namespace App\Http\Middleware;
 use App\Models\IpWhiteList;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckIpWhiteList
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-
         $ipAddress = $request->ip();
+
+
+        if (Cache::has('notified_ip_' . $ipAddress)) {
+            Log::info("IP $ipAddress sudah diberi notifikasi sebelumnya.");
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Access denied. You are not authorized to access this resource.',
+                'error' => 'ACCESS_DENIED'
+            ], 403);
+        }
 
         $Ipwhitelists = IpWhiteList::where('ip_address', $ipAddress)->first();
 
-        if(!$Ipwhitelists){
+        if (!$Ipwhitelists) {
             $this->sendTelegramNotification($ipAddress);
+
+            Cache::put('notified_ip_' . $ipAddress, true, now()->addSeconds(3));
+            Log::info("Notifikasi dikirim untuk IP: $ipAddress");
+
             return response()->json([
                 'status' => 'error',
                 'message' => 'Access denied. You are not authorized to access this resource.',
@@ -35,12 +44,13 @@ class CheckIpWhiteList
         return $next($request);
     }
 
-    private function sendTelegramNotification($ip){
-        $message = "⚠️ Unauthorized IP Detected: ". $ip;
+    private function sendTelegramNotification($ip)
+    {
+        $messages = "⚠️ Unauthorized IP Detected: " . $ip;
 
         $response = Http::post("https://api.telegram.org/bot" . env('TELEGRAM_BOT_TOKEN') . "/sendMessage", [
             'chat_id' => env('TELEGRAM_CHAT_ID'),
-            'text' => $message
+            'text' => $messages
         ]);
 
         if ($response->failed()) {
